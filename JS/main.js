b5ff0f76067a3734075af08ae75483f6f2166079
ennio <ennio.nick@gmail.com>
2025-01-16 16:50:05 +0100

    button functionality added

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+
+function openInstagramLink() {
+    window.open("https://www.instagram.com/schmenniooo/", "_blank");
+}
+
+function openGithubLink() {
+
+    window.open("https://github.com/schmenniooo", "_blank");
+}
+
+function openLinkedInLink() {
+
+    window.open("https://www.linkedin.com/in/ennio-schmidt-0304472b6/", "_blank");
+}
+
+function openEmailLink() {
+    window.open("mailto:ennio.nick@gmail.com", "_blank");
+}
