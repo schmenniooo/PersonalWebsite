1270bf36cd476e054a790d69bfddc74e1ee18cca
ennio <ennio.nick@gmail.com>
2025-02-08 18:14:52 +0100

    new approach

--- JS/main.js
+++ JS/main.js
@@ -1,20 +1,11 @@
 
-let target = "";
-const links = ["https://www.instagram.com/schmenniooo/", "https://github.com/schmenniooo", "https://www.linkedin.com/in/ennio-schmidt-0304472b6/", "mailto:ennio.nick@gmail.com"];
-
 document.querySelector('.buttonContainer').addEventListener('click', function (event) {
-
-    if (event.target.tagName === 'Button') {
-
+    if (event.target.tagName === 'BUTTON') {
         const action = event.target.getAttribute('data-action');
-
-        for (let i = 1; i < links.length; i++) {
-            if (action === links[i]) {
-                target = action
-            } else {
-                console.error("Error in links.")
-            }
+        if (action) {
+            window.open(action, '_blank');
+        } else {
+            console.error("No action defined for this button.");
         }
-        window.open(target, '_blank');
     }
 });
