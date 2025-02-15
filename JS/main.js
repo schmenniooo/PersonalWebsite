02663499060ff3dea8aa8a41a829830b0cbc315d
ennio <ennio.nick@gmail.com>
2025-02-15 11:13:07 +0100

    some fixes

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,15 @@
 
 
 document.addEventListener('DOMContentLoaded', function () {
+
     const buttonContainer = document.querySelector('.buttonContainer');
+
     if (buttonContainer) {
-        // If button clicked link is open by its data-action
+
         buttonContainer.addEventListener('click', function (event) {
+
             const button = event.target.closest('button');
+
             if (button) {
                 const action = button.getAttribute('data-action');
                 if (action) {
