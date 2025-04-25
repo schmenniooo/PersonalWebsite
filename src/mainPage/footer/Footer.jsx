6a6e9c12aa29acb033148c719df2484072314b21
ennio <ennio.nick@gmail.com>
2025-04-25 13:37:25 +0200

    importing images as variables

--- src/mainPage/footer/Footer.jsx
+++ src/mainPage/footer/Footer.jsx
@@ -1,6 +1,10 @@
 
-function Footer() {
+import instagramIcon from '../../assets/insta_icon.jpeg';
+import githubIcon from '../../assets/github_icon.jpeg';
+import linkedInIcon from '../../assets/linkedIn_icon.jpeg';
+import emailIcon from '../../assets/email_icon.jpeg';
 
+function Footer() {
     const currentYear = new Date().getFullYear().toString();
 
     return (
@@ -9,29 +13,29 @@ function Footer() {
                 <ul>
                     <li>
                         <a href="https://www.instagram.com/schmenniooo/" target="_blank">
-                            <img src="../../assets/insta_icon.jpeg" alt="instagram"/>
+                            <img src={instagramIcon} alt="instagram"/>
                         </a>
                     </li>
                     <li>
                         <a href="https://github.com/schmenniooo" target="_blank">
-                            <img src="../../assets/github_icon.jpeg" alt='github'/>
+                            <img src={githubIcon} alt='github'/>
                         </a>
                     </li>
                     <li>
                         <a href="https://www.linkedin.com/in/ennio-schmidt-0304472b6/" target="_blank">
-                            <img src="../../assets/linkedIn_icon.jpeg" alt='linkedin'/>
+                            <img src={linkedInIcon} alt='linkedin'/>
                         </a>
                     </li>
                     <li>
                         <a href="#">
-                            <img src="../../assets/email_icon.jpeg" alt='email'/>
+                            <img src={emailIcon} alt='email'/>
                         </a>
                     </li>
                 </ul>
                 <h3>&copy; {currentYear} Ennio Schmidt. All rights reserved.</h3>
             </footer>
         </div>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
