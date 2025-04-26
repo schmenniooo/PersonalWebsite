5a57a60c108a6a39832b34b84cb612e941b57672
ennio <ennio.nick@gmail.com>
2025-04-26 14:19:39 +0200

    reading content out of the yaml

--- src/defaultMainContainer/defaultMainContainer.jsx
+++ src/defaultMainContainer/defaultMainContainer.jsx
@@ -1,5 +1,21 @@
 
-function DefaultMainContainer(){
+import yaml from 'js-yaml'; // Use `import` instead of `require`
+
+async function readYamlFile(filePath) {
+    try {
+        const response = await fetch(filePath);
+        const fileContents = await response.text();
+        return yaml.load(fileContents);
+    } catch (e) {
+        console.error('Error reading YAML file:', e);
+        throw e;
+    }
+}
+
+function DefaultMainContainer() {
+
+    var currentYamlDate = readYamlFile('../src/assets/inputFiles/description.yaml');
+    console.log(currentYamlDate);
 
     return (
         <div className="mainContainer">
