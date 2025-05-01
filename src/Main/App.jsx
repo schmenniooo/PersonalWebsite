4d8a183f5ef52844b264387dfecd85c66037b327
ennio <ennio.nick@gmail.com>
2025-05-01 13:16:07 +0200

    Add Projects component to App
    Integrated the Projects component into the main App layout. This ensures that project-related content is now displayed alongside other sections like Header, CareerContent, and Footer.

--- src/Main/App.jsx
+++ src/Main/App.jsx
@@ -2,6 +2,7 @@
 import Header from '../header/Header.jsx'
 import DefaultMainContainer from '../defaultMainContainer/defaultMainContainer.jsx'
 import CareerContent from '../career/career.jsx'
+import Projects from '../projects/projects.jsx'
 import Footer from '../footer/Footer.jsx'
 
 function App() {
