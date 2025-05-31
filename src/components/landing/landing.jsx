866fb1fad5efddf24429e3cf05cf0392680f31dd
ennio <ennio.nick@gmail.com>
2025-05-31 17:18:26 +0200

    basic structure

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -2,8 +2,17 @@
 function Landing() {
 
     return (
-        <div>
+        <div className="landingContainer">
+            <div className="landingCard">
+                <section className="pictureContainer">
+                    <img className="picture" alt="Me"></img>
+                </section>
 
+                <h2 className="landigHeading">Welcome!</h2>
+                <p className="landingText">My name is Ennio and I'm Software Engineer</p>
+                <span className="counter">counter</span>
+                <button className="redirector">Go to Mainpage</button>
+            </div>
         </div>
     )
 }
