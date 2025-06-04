510238167d44dc76f1425e4b31132040369d12d5
ennio <ennio.nick@gmail.com>
2025-06-04 08:40:43 +0200

    defaultContainer files added

---
+++ src/components/model/defaultRouteContainer.jsx
@@ -0,0 +1,15 @@
+
+import './defaultRouteContainer.css'
+
+function DefaultRouteContainer() {
+
+    // Container
+    // Card
+    // Title
+    // Divider
+
+    // Loading ?
+    // Errors ?
+}
+
+export default DefaultRouteContainer;
