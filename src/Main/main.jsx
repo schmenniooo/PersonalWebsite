62458e6dd02562979f70e2c838663db341b4c82e
ennio <ennio.nick@gmail.com>
2025-06-04 08:36:10 +0200

    refactoring dir. structure

--- src/Main/main.jsx
+++ src/Main/main.jsx
@@ -4,8 +4,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import '../../index.css'
 import '../components/header/header.css'
-import '../components/main/defaultMainContainer.css'
-import '../components/career/career.css'
+import '../components/routes/main/defaultMainContainer.css'
+import '../components/routes/career/career.css'
 import '../components/footer/footer.css'
 
 createRoot(document.getElementById('root')).render(
