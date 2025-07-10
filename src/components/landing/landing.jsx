ad6cb2541be1100223c1c259ab5c3cedfe07248a
ennio <ennio.nick@gmail.com>
2025-07-10 19:17:23 +0200

    better text

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -15,8 +15,8 @@ function Landing({ onClose }) {
                 </section>
                 <div className="landing-divider"></div>
                 <h1 className="landingHeading">Welcome!</h1>
-                <p className="landingText">My name is Ennio and I'm a Software Engineer..</p>
-                <button className="redirector" onClick={goToMainPage}>Go ahead</button>
+                <p className="landingText">My name is Ennio Schmidt and I'm a Software Engineer..</p>
+                <button className="redirector" onClick={goToMainPage}>Explore My Work</button>
             </div>
         </div>
     )
