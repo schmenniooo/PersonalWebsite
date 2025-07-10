8f1f5a21bedcb1c1a0ad19cefa93b9f2650c5053
schmenniooo <166793900+schmenniooo@users.noreply.github.com>
2025-07-10 19:40:10 +0200

    dev (#45)
    * email possible
    
    * heading link added with appropriate css
    
    * better footer style
    
    * Add padding to content sections across various components
    
    Unified padding of 10px was added to `.paragraph-content`, `.career-content`, and `.project-content` classes. This improves spacing consistency and enhances the overall layout and visual appearance of these sections.
    
    * Add "My Skills" section with basic component and routing
    
    This commit introduces a new "My Skills" section in the navigation and creates a placeholder component (`Skills`) with corresponding routing. A `skills.yaml` file is added for future data population, though the component is not yet fully implemented.
    
    * data added
    
    * header switched
    
    * setup for fetching data from yaml file
    
    * safe
    
    * route for skills page
    
    * updated values
    
    * add basic structure for dynamically loading skills page
    
    * Refactor Skills component and improve code readability
    
    Standardized formatting and imports for consistency. Fixed dependencies in useEffect hook and corrected variable naming in skills mapping for better clarity. Added optional debugging log for skill data.
    
    * style classes added
    
    * moving loading spinner and error message style to global css
    
    * basic styling
    
    * Add skill category icons and improve skill item UI/UX
    
    Introduced dynamic category icons and enhanced the skill display. Updated the layout, added hover effects, and improved responsiveness for better user experience across devices.
    
    * removed underline of icon
    
    * Rename `/data` directory to `/inputData` and update references.
    
    Reorganized YAML file structure by renaming the `/data` folder to `/inputData` for improved clarity. Updated all file references and corresponding code to reflect this change, ensuring smooth functionality.
    
    * small fix
    
    * adding soft skills and descriptions
    
    * soft skills are now rendered
    
    * design for soft skills
    
    * certificate files added
    
    * Remove unused certificates and clean up debug logging.
    
    Deleted outdated certificate files from the public directory to reduce unnecessary clutter. Removed an optional debug comment in a console log statement to improve code clarity in the skills component.
    
    * landing files added
    
    * test envirement for landing
    
    * basic structure
    
    * some styling
    
    * some fixes
    
    * some fixes (again)
    
    * counter removed
    
    * better design
    
    * some fixes
    
    * responsive design
    
    * responsive design updates and button text change
    
    * landing page logic
    
    * new dir. structure
    
    * Revert "new dir. structure"
    
    This reverts commit 61d5726f2d6d4cdd6cb23fc97fc9c06312146666.
    
    * updated asset paths and moved 'logo.png' to 'public/images'
    
    * updated README.md
    
    * small fix
    
    * bug fix
    
    * google analytics script added
    
    * updated landing page styles
    
    * refactoring dir. structure
    
    * renaming
    
    * renaming
    
    * defaultContainer files added
    
    * iconBuilder js file for emoji specific functions
    
    * new dir. structure
    
    * refactoring
    
    * fix
    
    * icon builder prototype
    
    * two methods into one
    
    * model moved into routes dir.
    
    * emojig logic moved into iconBuilder.js
    
    * basic structure for routeContainer
    
    * css classes
    
    * using defaultRouteContainer.jsx as a wrapper object
    
    * landing css updated
    
    * new project added
    
    * some fixes in texts
    
    * link to fiverr acc added
    
    * updated link
    
    * new picture
    
    * old logo removed
    
    * Revert "old logo removed"
    
    This reverts commit bfa8c3744f40d1483f967dd87323a9a3f817d0eb.
    
    * picture fixed
    
    * picture in landing
    
    * better text
    
    * refactoring

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -11,12 +11,12 @@ function Landing({ onClose }) {
         <div className="landingContainer">
             <div className="landingCard">
                 <section className="pictureContainer">
-                    <img className="picture" src="/images/logo.png" alt="Me"></img>
+                    <img className="picture" src="/images/myself.jpeg" alt="Me"></img>
                 </section>
                 <div className="landing-divider"></div>
                 <h1 className="landingHeading">Welcome!</h1>
-                <p className="landingText">My name is Ennio and I'm a Software Engineer..</p>
-                <button className="redirector" onClick={goToMainPage}>Go ahead</button>
+                <p className="landingText">My name is Ennio Schmidt and I'm a Software Engineer..</p>
+                <button className="redirector" onClick={goToMainPage}>Explore My Work</button>
             </div>
         </div>
     )
